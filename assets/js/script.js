ca1d234ea5d97e67847c30712f3429ce13200cc9
EmeraldLiu <1309810@c126-24.ad.ccsd.net>
2017-05-30 11:18:00 -0700

    no map

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,19 +115,3 @@ $(document).ready(function() {
 
 });
 
-// ------------- GOOGLE MAP ------------ //
-function myMap() {
-  var myLatLng = {lat: 36.04, lng: -115.25};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: myLatLng
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Munchies World'
-  });
-}
-
